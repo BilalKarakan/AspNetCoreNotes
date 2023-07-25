e73ee137e3a63bbff3e3d7bda3c45830806403f9
Bilal Karakan <bilalkaraka36@gmail.com>
2023-07-26 02:47:51 +0300

    Folders listed

---
+++ mvc/Controllers/FolderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Mvc.Controllers
+{
+    public class FolderController : Controller
+    {
+        public IActionResult List()
+        {
+            DirectoryInfo directoryInfo = new(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"));
+            var folders = directoryInfo.GetDirectories();
+            return View(folders);
+        }
+    }
+}
