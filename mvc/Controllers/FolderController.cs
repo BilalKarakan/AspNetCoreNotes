988be193131bb94d44de76ccc911bf7b193c74a0
Bilal Karakan <bilalkaraka36@gmail.com>
2023-08-03 12:28:15 +0300

    Folders created

--- mvc/Controllers/FolderController.cs
+++ mvc/Controllers/FolderController.cs
@@ -11,5 +11,27 @@ namespace Mvc.Controllers
             var folders = directoryInfo.GetDirectories();
             return View(folders);
         }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string folderName)
+        {
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", folderName));
+            if (!info.Exists)
+            {
+                info.Create();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                TempData["key"] = "Aynı isimde klasör mevcur";
+            }
+            return View();
+        }
     }
 }
