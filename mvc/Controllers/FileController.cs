a1b0273243d55830c7acab07c39b37372dc085a8
Bilal Karakan <bilalkaraka36@gmail.com>
2023-08-31 01:25:00 +0300

    Files were listed

---
+++ mvc/Controllers/FileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Mvc.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult List()
+        {
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwroot", "files"));
+
+            var files = info.GetFiles();
+            return View(files);
+        }
+    }
+}
