36cef9fbb38211e1765d97932c7ae6cd2b378e23
Bilal Karakan <bilalkaraka36@gmail.com>
2023-09-02 16:41:03 +0300

    File was created

--- mvc/Controllers/FileController.cs
+++ mvc/Controllers/FileController.cs
@@ -7,10 +7,25 @@ namespace Mvc.Controllers
     {
         public IActionResult List()
         {
-            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwroot", "files"));
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files"));
 
             var files = info.GetFiles();
             return View(files);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string fileName)
+        {
+            FileInfo fileInfo = new(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", fileName));
+            if (!fileInfo.Exists)
+                fileInfo.Create();
+
+            return RedirectToAction("List");
         }
     }
 }
