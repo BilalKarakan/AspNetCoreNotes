9462ba5bf4bb3b43b0f12a42ca0fbc1b9653a2f9
Bilal Karakan <bilalkaraka36@gmail.com>
2023-09-05 21:34:04 +0300

    Files was uploaded

--- mvc/Controllers/FileController.cs
+++ mvc/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
 
@@ -31,7 +32,7 @@ namespace Mvc.Controllers
 
         public IActionResult CreateWithData()
         {
-            FileInfo info = new(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", Guid.NewGuid().ToString()));
+            FileInfo info = new(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", Guid.NewGuid().ToString()+".txt"));
 
             StreamWriter writer = info.CreateText();
 
@@ -40,6 +41,26 @@ namespace Mvc.Controllers
             writer.Close();
 
             return RedirectToAction("List");
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile formFile)
+        {
+            
+            var extension = Path.GetExtension(formFile.FileName);
+            var path = $"{Directory.GetCurrentDirectory()}/wwwroot/images/{Guid.NewGuid().ToString()}{extension}";
+            FileStream stream = new FileStream(path, FileMode.Create);
+            formFile.CopyTo(stream);
+
+            TempData["message"] = "Upload işlemi başarı ile gerçekleşmiştir.";
+
+
+            return RedirectToAction("Upload");
         }
     }
 }
