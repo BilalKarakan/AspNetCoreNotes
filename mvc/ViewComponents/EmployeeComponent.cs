8a6459a558b0ec62beaaf49caa2a46391b6acaae
Bilal Karakan <bilalkaraka36@gmail.com>
2023-09-12 14:23:23 +0300

    ViewComponents was added

---
+++ mvc/ViewComponents/EmployeeComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.Models;
+
+namespace Mvc.ViewComponents
+{
+    public class EmployeeComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(string role="Default")
+        {
+            var list = EmployeeContext.employees;
+
+            if (role == "Default")
+                return View(list);
+            else if (role == "Developer")
+                return View("Developer", list);
+            else
+                return View("Manager", list);
+
+        }
+    }
+}
