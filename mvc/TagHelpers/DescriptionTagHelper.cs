3ca0d3f86f35fd42f09e8ec066b4f10e17eee710
Bilal Karakan <bilalkaraka36@gmail.com>
2023-09-14 04:09:12 +0300

    Custom Tag Helper was created

---
+++ mvc/TagHelpers/DescriptionTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Mvc.TagHelpers
+{
+    [HtmlTargetElement("description")]
+    public class DescriptionTagHelper : TagHelper
+    {
+        public string FontType { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetHtmlContent($"<mark> Custom Tag Helper Works : {FontType} </mark>");
+        }
+    }
+}
